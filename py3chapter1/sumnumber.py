c01846debd0e5d59c0256ae5c3fa2091e700f267
Jia-HongHenryLee <honghenry.lee@gmail.com>
2017-01-01 22:58:27 +0800

    py3chapter1
    py3chapter1example

---
+++ py3chapter1/sumnumber.py
@@ -0,0 +1,26 @@
+
+print("Type integers and sum them")
+
+total=0
+count=0
+while True:
+	#line = input("Integer: ")
+	line = input()
+	if line:
+		try:
+			number = int(line)
+			total += number
+			count += 1         
+		except ValueError as err:
+			print(err)
+			continue
+		except EOFError:
+			break
+		#total += number
+		#count+=1
+	else:
+		break
+if count:
+	print("count: ",count,"total: ",total,"mean: ",total/count)
+
+
